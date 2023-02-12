dd2cce3214c6047c3ea7447f24ffc8c6e7f99269
KB-Toast <kevin.brusson@gmail.com>
2023-02-12 13:57:22 +0100

    directly modifying state using the immer library

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
@@ -10,38 +11,27 @@ const ADD_CUSTOM_VALUE = "add-custom-value";
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
+      state.count = state.count + 1;
+      return;
     case DECREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count - 1,
-      };
-
+      state.count = state.count - 1;
+      return;
     case SET_VALUE_TO_ADD:
-      return {
-        ...state,
-        valueToAdd: action.payload,
-      };
-
+      state.valueToAdd = action.payload;
+      return;
     case ADD_CUSTOM_VALUE:
-      return {
-        ...state,
-        count: state.count + state.valueToAdd,
-        valueToAdd: 0,
-      };
-
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
     default:
       // community convention to handle "default" either by throwing an err or by returning the state
       //throw new Error("unexpected action type: " + action.type);
-      return state;
+      return;
   }
 };
 
 function CounterPage({ initialCount }) {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
@@ -74,7 +64,6 @@ function CounterPage({ initialCount }) {
 
     dispatch({
       type: ADD_CUSTOM_VALUE,
-      payload: state.valueToAdd,
     });
   };
 
